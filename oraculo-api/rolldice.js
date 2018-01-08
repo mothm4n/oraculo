e8f5fa84a345247ad45ef344f708e041892c0c6b
Marc Gelpi Mateu <ggelpi@gmail.com>
2018-01-08 11:45:53 +0100

    added various changes and tests, first "service" that is a rolldice.

---
+++ oraculo-api/rolldice.js
@@ -0,0 +1,18 @@
+import rollDice from "roll-dice";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+
+  const d = new rollDice();
+  var diceType = event.pathParameters.dice;
+  var result = d.roll(diceType);
+
+  
+  // noteId: event.pathParameters.id
+
+  try {
+    callback(null, success(result));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
